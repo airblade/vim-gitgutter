d24686e1bcbe85010e66cb13d4d629a14d089e2b
Andy Stewart <boss@airbladesoftware.com>
2014-11-13 14:41:26 +0100

    Extract name manipulation.

--- test/test.sh
+++ test/test.sh
@@ -2,13 +2,19 @@
 
 # TODO: exit with non-zero status code when tests fail.
 
+canonicalise_test_name() {
+  local testname=$1                                              # testFoo.vim
+  name=${testname%.*}                                            # testFoo
+  name=${name:4}                                                 # Foo
+  name="$(tr '[:upper:]' '[:lower:]' <<< ${name:0:1})${name:1}"  # foo
+}
+
+
 rm -f *.out
 
 for testcase in test*.vim; do
-  vim -N -u NONE -S $testcase -c 'quit!'                        # testFoo.vim
-  testname=${testcase%.*}                                       # testFoo
-  name=${testname:4}                                            # Foo
-  name="$(tr '[:upper:]' '[:lower:]' <<< ${name:0:1})${name:1}" # foo
+  vim -N -u NONE -S $testcase -c 'quit!'
+  canonicalise_test_name $testcase
   expected=$name.ok
   actual=$name.out
   diff $expected $actual && echo "$name ok" || echo "$name failed"
